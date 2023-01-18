0027fe570cb5a4cd924cbbe839fd61bb68b6773a
Dsblima <dsbl@ecomp.poli.br>
2023-01-18 08:02:48 -0300

    Update Assets, create Loading component, adjust status bar and add fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { 
+        useFonts, 
+        Inter_400Regular, 
+        Inter_600SemiBold, 
+        Inter_700Bold, 
+        Inter_800ExtraBold  
+      } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
-  return (
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return (<Loading />)
+  }
+
+  return (    
     <View style={styles.container}>
-      <Text>Consegui essa porra!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text} >Consegui essa porra!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </View>
   );
 }
@@ -13,8 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c02da7',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#FFF',
+    fontFamily: 'Inter_800ExtraBold'
+  }
 });
