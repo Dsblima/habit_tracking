07a08903169be0d30f4d4271b665c656a007997c
Dsblima <dsbl@ecomp.poli.br>
2023-01-21 00:55:42 -0300

    Create summary table

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
-import { Habit } from "./components/Habit"
+import './styles/global.css'
+import { Plus } from 'phosphor-react'
 
-function App() {
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
+
+export function App() {
  
   return (
-    <div>
-      <Habit completed={2} />
-      <Habit completed={3} />
-      <Habit completed={7} />
-      <Habit completed={3} />
-      <Habit completed={4} />
-      <Habit completed={5} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
