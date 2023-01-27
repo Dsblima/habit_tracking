fc2471192f2e62dc816aabddecd69ce588d50ad9
Dsblima <dsbl@ecomp.poli.br>
2023-01-26 23:05:53 -0300

    Add animations and transitions

--- web/src/components/HabitList.tsx
+++ web/src/components/HabitList.tsx
@@ -52,7 +52,7 @@ export function HabitList({ date, onCompletedChanged }: HabitListProps) {
   }
 
   return (
-    <div className='mt-6 flex flex-col gap-3'>
+    <div className='mt-6 -ml-5 flex flex-col gap-3'>
       {
         habitInfo?.possibleHabits.map(habit => {
           return (
@@ -61,9 +61,9 @@ export function HabitList({ date, onCompletedChanged }: HabitListProps) {
               defaultChecked={habitInfo.completedHabits.includes(habit.id)}
               onCheckedChange={() => handleToggleHabit(habit.id)}
               disabled={isBeforaToday}
-              className='flex items-center gap-3 group'
+              className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
             >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500  transition-colors group-focus:ring-2 group-focus:ring-violet-500 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -64,15 +64,15 @@ export function NewHabitForm() {
         Qual a frequência?
       </label>
 
-      <div className="flex flex-col gap-2 mt-3">
+      <div className="flex flex-col gap-2 mt-3 -ml-5">
         {availableWeekDays.map((weekDay, index) => (
           <Checkbox.Root
             key={weekDay}
-            className='flex items-center gap-3 group'
+            className='flex items-center gap-3 group focus:outline-none '
             onCheckedChange={()=> handleToggleWeekDay(index)}
             checked={weekDays.includes(index)}
           >
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500  transition-colors group-focus:ring-2 group-focus:ring-violet-500 group-focus:ring-offset-2 group-focus:ring-offset-background'>
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -87,7 +87,7 @@ export function NewHabitForm() {
 
       <button 
         type="submit" 
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
